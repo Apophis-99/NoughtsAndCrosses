e279bb10e00ca92d2839a4b23017531f4ef9d9c9
AS-99-Parkinson <Apophis-99@users.noreply.github.com>
2022-09-25 22:26:48 +0100

    ComputerAI system improved

--- Game/ComputerAI.cs
+++ Game/ComputerAI.cs
@@ -3,17 +3,31 @@
 public class ComputerAi
 {
     private List<List<SlotState>> _boardState = new();
-    private SlotState _playerPiece;
+    private readonly SlotState _playerPiece;
+    private readonly SlotState _computerPiece;
 
     public ComputerAi(SlotState playerPiece)
     {
         _playerPiece = playerPiece;
+        _computerPiece = (_playerPiece == SlotState.X ? SlotState.O : SlotState.X);
     }
     
     public int CalculateNextMove(List<List<SlotState>> boardState)
     {
         _boardState = boardState;
-        return IsFirstTurn() ? GetRandom() : GetPlayerBlock();
+
+        if (IsFirstTurn())
+            return GetRandom();
+        
+        var result = GetWinningMove();
+        if (result > 0)
+            return result;
+        
+        result = GetPlayerBlock();
+        if (result > 0)
+            return result;
+
+        return GetRandom();
     }
 
     private bool IsFirstTurn()
@@ -36,14 +50,99 @@ public class ComputerAi
     {
         for (var i = 0; i < 3; i++)
         {
-            if (_boardState[i][0] == _playerPiece && _boardState[i][1] == _playerPiece)
+            if (_boardState[i][0] == _playerPiece && _boardState[i][1] == _playerPiece && _boardState[i][2] == SlotState.Empty)
                 return i * 3 + 1 + 2;
-            if (_boardState[i][0] == _playerPiece && _boardState[i][2] == _playerPiece)
+            if (_boardState[i][0] == _playerPiece && _boardState[i][2] == _playerPiece && _boardState[i][1] == SlotState.Empty)
                 return i * 3 + 1 + 1;
-            if (_boardState[i][1] == _playerPiece && _boardState[i][2] == _playerPiece)
+            if (_boardState[i][1] == _playerPiece && _boardState[i][2] == _playerPiece && _boardState[i][0] == SlotState.Empty)
                 return i * 3 + 1 + 0;
+                
+            if (_boardState[0][i] == _playerPiece && _boardState[1][i] == _playerPiece && _boardState[2][i] == SlotState.Empty)
+                return 2 * 3 + 1 + i;
+            if (_boardState[0][i] == _playerPiece && _boardState[2][i] == _playerPiece && _boardState[1][i] == SlotState.Empty)
+                return 1 * 3 + 1 + i;
+            if (_boardState[1][i] == _playerPiece && _boardState[2][i] == _playerPiece && _boardState[0][i] == SlotState.Empty)
+                return 0 * 3 + 1 + i;
         }
         
-        return 1;
+        if (_boardState[0][0] == _playerPiece && _boardState[1][1] == _playerPiece && _boardState[2][2] == SlotState.Empty)
+            return 9;
+        if (_boardState[0][0] == _playerPiece && _boardState[2][2] == _playerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+        if (_boardState[1][1] == _playerPiece && _boardState[2][2] == _playerPiece && _boardState[0][0] == SlotState.Empty)
+            return 1;
+        
+        if (_boardState[0][2] == _playerPiece && _boardState[1][1] == _playerPiece && _boardState[2][0] == SlotState.Empty)
+            return 7;
+        if (_boardState[0][2] == _playerPiece && _boardState[2][0] == _playerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+        if (_boardState[2][0] == _playerPiece && _boardState[1][1] == _playerPiece && _boardState[0][2] == SlotState.Empty)
+            return 3;
+
+        return 0;
+    }
+
+    private int GetWinningMove()
+    {
+        // Horizontal
+        if (_boardState[0][0] == _computerPiece && _boardState[0][1] == _computerPiece && _boardState[0][2] == SlotState.Empty)
+            return 3;
+        if (_boardState[0][1] == _computerPiece && _boardState[0][2] == _computerPiece && _boardState[0][0] == SlotState.Empty)
+            return 0;
+        if (_boardState[0][0] == _computerPiece && _boardState[0][2] == _computerPiece && _boardState[0][1] == SlotState.Empty)
+            return 2;
+        
+        if (_boardState[1][0] == _computerPiece && _boardState[1][1] == _computerPiece && _boardState[1][2] == SlotState.Empty)
+            return 6;
+        if (_boardState[1][1] == _computerPiece && _boardState[1][2] == _computerPiece && _boardState[1][0] == SlotState.Empty)
+            return 4;
+        if (_boardState[1][0] == _computerPiece && _boardState[1][2] == _computerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+        
+        if (_boardState[2][0] == _computerPiece && _boardState[2][1] == _computerPiece && _boardState[2][2] == SlotState.Empty)
+            return 9;
+        if (_boardState[2][1] == _computerPiece && _boardState[2][2] == _computerPiece && _boardState[2][0] == SlotState.Empty)
+            return 7;
+        if (_boardState[2][0] == _computerPiece && _boardState[2][2] == _computerPiece && _boardState[2][1] == SlotState.Empty)
+            return 8;
+        
+        // Vertical
+        if (_boardState[0][0] == _computerPiece && _boardState[1][0] == _computerPiece && _boardState[2][0] == SlotState.Empty)
+            return 7;
+        if (_boardState[1][0] == _computerPiece && _boardState[2][0] == _computerPiece && _boardState[0][0] == SlotState.Empty)
+            return 1;
+        if (_boardState[0][0] == _computerPiece && _boardState[2][0] == _computerPiece && _boardState[1][0] == SlotState.Empty)
+            return 4;
+        
+        if (_boardState[0][1] == _computerPiece && _boardState[1][1] == _computerPiece && _boardState[2][1] == SlotState.Empty)
+            return 8;
+        if (_boardState[1][1] == _computerPiece && _boardState[2][1] == _computerPiece && _boardState[0][1] == SlotState.Empty)
+            return 2;
+        if (_boardState[0][1] == _computerPiece && _boardState[2][1] == _computerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+        
+        if (_boardState[0][2] == _computerPiece && _boardState[1][2] == _computerPiece && _boardState[2][2] == SlotState.Empty)
+            return 9;
+        if (_boardState[1][2] == _computerPiece && _boardState[1][2] == _computerPiece && _boardState[2][2] == SlotState.Empty)
+            return 3;
+        if (_boardState[0][2] == _computerPiece && _boardState[1][2] == _computerPiece && _boardState[2][2] == SlotState.Empty)
+            return 6;
+        
+        // Diagonal
+        if (_boardState[0][0] == _computerPiece && _boardState[1][1] == _computerPiece && _boardState[2][2] == SlotState.Empty)
+            return 9;
+        if (_boardState[1][1] == _computerPiece && _boardState[2][2] == _computerPiece && _boardState[0][0] == SlotState.Empty)
+            return 1;
+        if (_boardState[0][0] == _computerPiece && _boardState[2][2] == _computerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+        
+        if (_boardState[0][2] == _computerPiece && _boardState[1][1] == _computerPiece && _boardState[2][0] == SlotState.Empty)
+            return 7;
+        if (_boardState[1][1] == _computerPiece && _boardState[2][0] == _computerPiece && _boardState[0][2] == SlotState.Empty)
+            return 3;
+        if (_boardState[0][2] == _computerPiece && _boardState[2][0] == _computerPiece && _boardState[1][1] == SlotState.Empty)
+            return 5;
+
+        return 0;
     }
 }
